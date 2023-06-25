a0195cb5940c730e9d633f31a54664c35acb5a71
Karik Isichei <karik.isichei@gmail.com>
2023-06-25 11:31:00 +0100

    First dump of basic search and homepage

---
+++ README.md
@@ -0,0 +1,10 @@
+# A Simple Personal Recipe Book App
+
+The idea of this is to have a FASTAPI App using htmx to search through recipes that I have stored in S3.
+
+## Ideas:
+- Could run on Rasberry Pi instead of cloud?
+
+## Notes:
+- [Good fastapi example](https://github.com/renceInbox/fastapi-todo)
+- [Youtube tutorial](https://youtu.be/CWSmP3LSILo) | ([code](https://github.com/PrettyPrinted/youtube_video_code/tree/master/2022/03/30/Dynamic%20Web%20Pages%20Without%20JavaScript%3F%20-%20Intro%20to%20HTMX))
---
+++ main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+RECIPES_DB = [
+    {
+        "name": "chicken-dhansak-recipe",
+        "description": "A chicken dhansak recipe from BBC good foods",
+        "source": "recipes/chicken-dhansak-recipe.pdf",
+    },
+    {
+        "name": "christmas-roast-potatoes",
+        "description": "A jamie oliver roast potato recipe usually used at xmas",
+        "source": "recipes/jamie-oliver-roast-potatoes.pdf",
+    },
+]
+
+def _search_recipes(text:str) -> list[dict]:
+    if not text:
+        return RECIPES_DB[:10]
+
+    for item in RECIPES_DB:
+        matched = [
+            item
+            for item in RECIPES_DB
+            if any(
+                [word in item["description"].lower() for word in text.lower().split()]
+            )
+        ]
+        return matched
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    context = {
+        "request": request,
+        "results": _search_recipes(""),
+    }
+    return templates.TemplateResponse("home.html", context=context)
+
+
+@app.get("/search-recipes", response_class=HTMLResponse)
+def search_recipes(request: Request, text: str):
+    context = {
+        "request": request,
+        "results": _search_recipes(text),
+    }
+    return templates.TemplateResponse("search_results.html", context=context)
